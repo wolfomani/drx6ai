833ac59fcc8335c49ab3f49da2cac1f5c09b6174
3ZWOORD <balqees0alalawi@gmail.com>
2025-07-24 06:16:24 +0400

    Add files via upload
    Signed-off-by: 3ZWOORD  <balqees0alalawi@gmail.com>

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+export default function FixedComponent() {
+  import { type ClassValue, clsx } from "clsx"
+  import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+  }
 }
