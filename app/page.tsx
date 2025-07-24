f86228c436add9a3fbfdbb8b7da98289fd4f0703
3ZWOORD <balqees0alalawi@gmail.com>
2025-07-24 05:35:08 +0400

    Update page.tsx
    Signed-off-by: 3ZWOORD  <balqees0alalawi@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import CognitiveChatInterface from "@/components/chat/cognitive-chat-interface";
-
-export default function Home() {
-  return (
-    <main className="h-screen">
-      <CognitiveChatInterface />
-    </main>
-  );
+export default function HomePage() {
+  import CognitiveChatInterface from "@/components/chat/cognitive-chat-interface";
+  
+  export default function Home() {
+    return (
+      <main className="h-screen">
+        <CognitiveChatInterface />
+      </main>
+    );
+  }
 }
